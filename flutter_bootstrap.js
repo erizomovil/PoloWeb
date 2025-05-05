26ef16d4c5be67808618b3008247f3e7e0d03fe3
Erizomovil <jg.arcalagonzalez@gmail.com>
2025-05-05 14:49:01 +0100

    second commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4292389390"
+    serviceWorkerVersion: "3365345344"
   }
 });
